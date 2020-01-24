3375f49a85362b0b405b40fdf03b4e4ee63f53ff
Malaji Nagaraju <malaji.nagaraju@gmail.com>
2020-01-24 17:31:45 +0530

    pushing through master branch

---
+++ build/precache-manifest.d42e4d07ad84f6dc3af70e72d325f492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b14e6f39bf283c525aac44858fa6c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1afaaa87529c80a839a",
+    "url": "/static/css/main.7e660580.chunk.css"
+  },
+  {
+    "revision": "a16d0aaa537138b02342",
+    "url": "/static/js/2.c3a539a1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c3a539a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1afaaa87529c80a839a",
+    "url": "/static/js/main.4db3cb53.chunk.js"
+  },
+  {
+    "revision": "f2b76d27e28f871e85d5",
+    "url": "/static/js/runtime-main.869c95a1.js"
+  }
+]);
